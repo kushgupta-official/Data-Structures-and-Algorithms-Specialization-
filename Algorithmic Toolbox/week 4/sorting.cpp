eac31a908e8b006de7933ee78fb0804ab563f808
kushgupta-official <kushgupta.potus@gmail.com>
2020-05-22 04:35:43 +0530

    Commited

--- Algorithmic Toolbox/week 4/sorting.cpp
+++ Algorithmic Toolbox/week 4/sorting.cpp
@@ -1,45 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-
+using namespace std;
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void partition3(int a[], int low, int high, int &i, int &j){
+    if (high - low <= 1){
+        if (a[high] < a[low])
+            swap(&a[high], &a[low]);
+        i = low;
+        j = high;
+        return;
     }
-  }
-  swap(a[l], a[j]);
-  return j;
+    int mid = low;
+    int pivot = a[high];
+    while (mid <= high){
+        if (a[mid] < pivot)
+            swap(&a[low++], &a[mid++]);
+        else if (a[mid] == pivot)
+            mid++;
+        else if (a[mid] > pivot)
+            swap(&a[mid], &a[high--]);
+    }
+    i = low-1;
+    j = mid;
 }
-
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
-  }
-
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
-
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+void quicksort(int a[], int low, int high){
+    if (low>=high)
+        return;
+    int i, j;
+    partition3(a, low, high, i, j);
+    quicksort(a, low, i);
+    quicksort(a, j, high);
 }
-
-int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  randomized_quick_sort(a, 0, a.size() - 1);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cout << a[i] << ' ';
-  }
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    quicksort(a, 0, n-1);
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
--- Algorithmic Toolbox/week 6/placing_parentheses.cpp
+++ Algorithmic Toolbox/week 6/placing_parentheses.cpp
@@ -2,29 +2,70 @@
 #include <cassert>
 #include <string>
 #include <vector>
+#include <limits>
 
-using std::vector;
-using std::string;
-using std::max;
-using std::min;
+using namespace std;
 
-long long eval(long long a, long long b, char op) {
-  if (op == '*') {
-    return a * b;
-  } else if (op == '+') {
-    return a + b;
-  } else if (op == '-') {
-    return a - b;
-  } else {
-    assert(0);
-  }
+#define long long long
+long eval(long a, long b, char op) {
+    if (op == '*'){
+        return a * b;
+    }
+  else if (op == '+'){
+        return a + b;
+    }
+  else if (op == '-'){
+        return a - b;
+    }
+  else {
+        assert(0);
+    }
 }
+long get_maximum_value(const string &exp) {
+    vector<long >numbers;
+    vector<char >operators;
+    for (int i = 0; i <  exp.size(); i++){
+        if (i%2==0){
+            numbers.push_back((long ) exp[i] - 48);
+        }
+    else{
+            operators.push_back(exp[i]);
+        }
+    }
+    vector<vector<long > >M(numbers.size());
+    vector<vector<long > >m(numbers.size());
+    for (int i=0; i< numbers.size(); i++){
+        M[i].resize(numbers.size());
+        m[i].resize(numbers.size());
+    }
+    for (int i = 0; i < numbers.size(); ++i) {
+        for (int j = 0; j < numbers.size(); ++j) {
+            if (i==j){
+                M[i][j] = numbers[i];
+                m[i][j] = numbers[i];
+            }
+        }
+    }
+    for (int s=1; s < numbers.size(); s++){
+        for (int i=0; i < numbers.size()-s; i++){
+            int j = i+s;
+            long max_value = numeric_limits<long>::min();
+            long min_value = numeric_limits<long>::max();
+            for (int k = i; k <=  j-1; k++){
+                long a = eval(M[i][k], M[k+1][j], operators[k]);
+                long b = eval(M[i][k], m[k+1][j], operators[k]);
+                long c = eval(m[i][k], M[k+1][j], operators[k]);
+                long d = eval(m[i][k], m[k+1][j], operators[k]);
+                max_value = max(max_value,max(a, max(b, max(c, d))));
+                min_value = min(min_value,min(a, min(b, min(c, d))));
+            }
 
-long long get_maximum_value(const string &exp) {
-  //write your code here
-  return 0;
+            m[i][j] = min_value;
+            M[i][j] = max_value;
+        }
+    }
+    return M[0][numbers.size()-1];
 }
-
 int main() {
   string s;
   std::cin >> s;
