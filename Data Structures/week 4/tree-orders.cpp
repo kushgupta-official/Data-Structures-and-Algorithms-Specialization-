4cfaca374ad70a06cc398c036a442945b950b2c6
kushgupta-official <kushgupta.potus@gmail.com>
2020-06-09 20:49:40 +0530

    commited

---
+++ Data Structures/week 4/tree-orders.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+
+class TreeOrders {
+  int n;
+  vector <int> key;
+  vector <int> left;
+  vector <int> right;
+
+public:
+  void read() {
+    cin >> n;
+    key.resize(n);
+    left.resize(n);
+    right.resize(n);
+    for (int i = 0; i < n; i++) {
+      cin >> key[i] >> left[i] >> right[i];
+    }
+  }
+
+
+  vector <int> in_order() {
+    vector<int> result;
+    stack <int>s;
+    int index=0;
+    while (!s.empty() || index!=-1){
+    	if (index!=-1){
+    		s.push(index);
+    		index=left[index];
+    	}
+    	else{
+    		index=s.top();
+    		s.pop();
+    		result.push_back(key[index]);
+    		index=right[index];
+    	}
+    }
+    return result;
+  }
+
+  vector <int> pre_order() {
+    vector<int> result; 
+    stack <int>s;
+    s.push(0);
+    while(!s.empty()){
+    	int index=s.top();
+    	s.pop();
+    	result.push_back(key[index]);
+
+    	if(right[index]!=-1){
+    		s.push(right[index]);
+    	}
+    	if(left[index]!=-1){
+    		s.push(left[index]);
+    	}
+    }    
+    return result;
+  }
+
+  vector <int> post_order() {
+    vector<int> result;
+    stack <int>s;
+    // int index=0;
+    // do{
+    // 	while(index!=-1){
+    // 		if(right[index]!=-1){
+    // 			s.push(right[index]);
+    // 		}
+    // 		s.push(index);
+    // 		index=left[index];
+    // 	}
+
+    // 	index=s.top();
+    // 	s.pop();
+
+    // 	if (right[index]!=-1 && s.top()==right[index]){
+    // 		s.pop();
+    // 		s.push(index);
+    // 		index=right[index];
+    // 	}
+    // 	else{
+    // 		result.push_back(index);
+    // 		index=-1;
+    // 	}
+    // }while(!s.empty());
+    int lastVisitedNode=-1;
+    int index=0;
+    while(!s.empty() || index!=-1){
+    	if(index!=-1){
+    		s.push(index);
+    		index=left[index];
+    	}
+    	else{
+    		int topNode=s.top();
+    		if (right[topNode]!=-1 && lastVisitedNode!=right[topNode]){
+    			index=right[index];
+    			
+    		}
+    		else{
+    			result.push_back(topNode);
+    			lastVisitedNode=s.top();
+    			s.pop();
+    		}
+    	}
+    	cout<<key[s.top()]<<endl;
+    }
+    return result;
+  }
+};
+
+void print(vector <int> a) {
+  for (size_t i = 0; i < a.size(); i++) {
+    if (i > 0) {
+      cout << ' ';
+    }
+    cout << a[i];
+  }
+  cout << '\n';
+}
+
+
+int main (int argc, char **argv)
+{
+  //ios_base::sync_with_stdio(0);
+  TreeOrders t;
+  t.read();
+  print(t.in_order());
+  print(t.pre_order());
+  print(t.post_order());
+  return 0;
+}
+
