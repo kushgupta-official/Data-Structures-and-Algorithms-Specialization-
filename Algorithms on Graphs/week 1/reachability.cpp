7394e7bffefd966e0a630935233a6758bc01708e
kushgupta-official <kushgupta.potus@gmail.com>
2020-08-20 02:25:31 +0530

    reachibility

--- Algorithms on Graphs/week 1/reachability.cpp
+++ Algorithms on Graphs/week 1/reachability.cpp
@@ -1,26 +1,55 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
 using std::vector;
 using std::pair;
 
+std::vector<bool> visited(10000,0);
+std::vector<int> res;
 
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
+void kush_gupta(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+}
+
+void explore(vector<vector<ll> > &adj, ll x){
+  visited[x]=1;
+  res.push_back(x);
+  for (ll i=0;i<adj[x].size();i++){
+    if(visited[adj[x][i]]==0){
+      explore(adj,adj[x][i]);
+    }
+  }
+}
+
+ll reach(vector<vector<ll> > &adj, ll x, ll y) {
+  explore(adj,x);
+  for (ll i=0;i<res.size();i++){
+    if(res[i]==y){
+      return 1;
+    }
+  }
   return 0;
 }
 
 int main() {
-  size_t n, m;
+  kush_gupta();
+  ll n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
-    int x, y;
+  vector<vector<ll> > adj(n+2);
+  for (ll i = 0; i < m; i++) {
+    ll x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x].push_back(y);
+    adj[y].push_back(x);
   }
-  int x, y;
+  ll x, y;
   std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+  std::cout << reach(adj,x,y);
+  return 0;
 }
